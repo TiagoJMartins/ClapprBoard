243b48c4a9037666db98ee26c6e72bba9057c7ba
TiagoJMartins <tiagodksp@gmail.com>
2016-04-23 19:04:28 +0100

    Improve boilerplate.

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+var config = {
+  BOOTSTRAP: 'src/bower_components/bootstrap-sass/assets/stylesheets',
+  SRC_SASS: 'src/sass/**/*.scss',
+  STYLES_OUTPUT: 'client/css'
+}
+
+gulp.task('css', function() {
+  gulp.src(config.SRC_SASS)
+      .pipe(plumber())
+      .pipe(sass({
+        includePaths: [config.BOOTSTRAP]
+      }))
+      .pipe(gulp.dest(config.STYLES_OUTPUT));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(config.SRC_SASS, ['css']);
+});
+
+gulp.task('default', ['css']);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var logger = require('morgan');
 
 var app = module.exports = loopback();
+
+app.use(logger('dev'));
 
 app.start = function() {
   // start the web server
