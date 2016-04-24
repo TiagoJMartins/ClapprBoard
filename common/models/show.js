0c5091d9523739a9eddacb7d5554f3d9111a15b1
TiagoJMartins <tiagodksp@gmail.com>
2016-04-24 19:09:04 +0100

    Remove unused module imports.

--- common/models/show.js
+++ common/models/show.js
@@ -1,10 +1,5 @@
-var request = require('request');
-var async = require('async');
-var xml2js = require('xml2js');
-
 module.exports = function(Show) {
   Show.beforeCreate = function(next, modelInstance) {
-    var API_KEY = '3CD608E0AEEFAEA6';
 
     next();
   };
