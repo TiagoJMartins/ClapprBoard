fab720b4b3cb23ba1cb88b02d65b4c65e14dbbf6
TiagoJMartins <tiagodksp@gmail.com>
2016-05-01 20:13:25 +0100

    Add 'My Shows' basic content.

--- client/app/controllers/myshows.js
+++ client/app/controllers/myshows.js
@@ -3,6 +3,7 @@ angular.module('MainApp')
 	function($scope, WatchListService, Show, $rootScope){
 	
 		$scope.subscriptions = {};
+		$scope.images = {};
 		
 		WatchListService
 		.shows()
@@ -13,6 +14,27 @@ angular.module('MainApp')
 			}
 
 			$scope.subscriptions = subShows.result;
+			var showArray = Object.keys($scope.subscriptions);
+
+			async.forEach(showArray, function(show, callback) {
+				if (show.length) {
+					Show.findOne({
+						filter: {
+							where: {
+								slug: show
+							},
+							fields: {
+								images: true
+							}
+						}
+					}, function(inst) {
+						$scope.images[show] = inst.images;
+					})
+				}
+				callback();
+			}, function(err) {
+				if (err) return console.log(err);
+			});
 		})
 		.catch(function(err) {
 			console.log(err);
